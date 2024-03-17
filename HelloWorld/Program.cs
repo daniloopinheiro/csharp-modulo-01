aa97f28e12a930ba99a8946c75f5f1b6a606caa9
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-17 15:02:10 +0000

    Operacoes Com Texto v7

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -9,7 +9,7 @@ class Program
 
         string paragrafo = texto1 + " " + 7 + " " + true + " " + texto2;
 
-        string caminho = "C:\neste";
+        string caminho = "C:\\teste";
 
         Console.WriteLine(paragrafo);
     }
