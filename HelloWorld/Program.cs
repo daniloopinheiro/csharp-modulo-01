c43a7190ad523f6ed33777761ca885dc5ab03f2e
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-17 15:13:50 +0000

    Operacoes Com Texto v13

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,20 +4,8 @@ class Program
 {
     static void Main()
     {
-        string texto1 = "A primeira frase.";
-        string texto2 = "Segunda frase";
+        string texto = "O usuario Welisson gosta do numero 7";
 
-        string paragrafo = texto1 + " " + 7 + " " + true + " " + texto2;
-
-        string paragrafo2 = $"A primeira frase. {7} {true} segunda frase.";
-
-        StringBuilder stringBuilder = new StringBuilder();
-
-        stringBuilder.Append(paragrafo);
-        stringBuilder.Append(paragrafo2);
-
-        string resultado = stringBuilder.ToString();
-
-        Console.WriteLine(resultado);
+        Console.WriteLine(texto);
     }
 }
