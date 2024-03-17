f53ec37ce1f21f8601173eb4d887b9219d5bc9e8
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-17 15:23:09 +0000

    Datas v4

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         DataOnly dia = new DataOnly(2023, 12, 1);
 
-        string diaEmTexto = dia.ToShortDateString();
+        string diaEmTexto = dia.ToLongDateString();
 
         Console.WriteLine(dia);
     }
