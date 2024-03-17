f915b332f91121cc55c0c550d6d7c7fe346c3915
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-17 15:26:43 +0000

    Datas v7

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ class Program
     {
         DataOnly dia = new DataOnly(2023, 12, 1);
 
-        string diaEmTexto = dia.ToString(new CultureInfo("pt-BR"));
+        string diaEmTexto = dia.ToString("D", new CultureInfo("pt-BR"));
 
         Console.WriteLine(dia);
     }
