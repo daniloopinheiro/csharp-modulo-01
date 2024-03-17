ec7d2859d9cbb4cc5d79e1424f26887cf020c95f
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-17 14:31:23 +0000

    Tipos Texto v3

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -12,8 +12,10 @@ class Program
         string texto = "Este curso é muito bom";
         char primeiraLetraDoTexto = texto[0];
 
-        string meuNome = "            Wellison              ";
+        string meuNome = "            Wellison Arley             ";
+        string nomeSemEspaco = meuNome.Trim();
 
         Console.WriteLine(meuNome);
+        Console.WriteLine(nomeSemEspaco);
     }
 }
