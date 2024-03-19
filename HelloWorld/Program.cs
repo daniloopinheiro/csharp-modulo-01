cade348b592e381a9c998c86a7a4a014ce22318c
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-19 15:37:06 +0000

    Objects

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,12 +4,8 @@ class Program
 {
     static void Main()
     {
-        string nome = "Welisson Arley";
-        int meuNumero = 7;
+        var minhaVariavel = new Carro();
 
-        var nome2 = "Welisson Arley";
-
-        string meuSobrenome;
-        var variavel = DateTime.Now;
+        minhaVariavel.
     }
 }
