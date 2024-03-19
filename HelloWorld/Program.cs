676225937788ab573de68c34c993927766bbbe2b
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-19 16:35:25 +0000

    Listas

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,14 +4,15 @@ class Program
 {
     static void Main()
     {
-        int[,] inteiros = new int[10, 10];
+        List<int> inteiros = new List<int>();
 
-        inteiros[0,0] = 1;
-        inteiros[0, 3] = 2;
+        inteiros.Add(1);
+        inteiros.Add(2);
 
-        Console.WriteLine(inteiros[0,0]);
-        Console.WriteLine(inteiros[0,1]);
-        Console.WriteLine(inteiros[0,2]);
-        Console.WriteLine(inteiros[0,3]);
+        inteiros.Remove(0);
+
+        inteiros.Clear();
+
+        Console.WriteLine(inteiros.Count);
     }
 }
