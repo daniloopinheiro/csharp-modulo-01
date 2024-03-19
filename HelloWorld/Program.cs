c428b6fea5fa4477ca5c7a9440e4a373ae319a0c
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-19 16:32:29 +0000

    Arrays

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,8 +4,14 @@ class Program
 {
     static void Main()
     {
-        int? idade = null;
+        int[,] inteiros = new int[10, 10];
 
-        bool informouIdade = idade.HasValue;
+        inteiros[0,0] = 1;
+        inteiros[0, 3] = 2;
+
+        Console.WriteLine(inteiros[0,0]);
+        Console.WriteLine(inteiros[0,1]);
+        Console.WriteLine(inteiros[0,2]);
+        Console.WriteLine(inteiros[0,3]);
     }
 }
