cfd5e722ca136fb8be71838646c3ca447e525ec1
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-20 18:49:48 +0000

    Static

--- HelloWorld/OperacoesMatematicas.cs
+++ HelloWorld/OperacoesMatematicas.cs
@@ -1,17 +1,7 @@
 namespace HelloWorld;
 
-public class OperacoesMatematicas
+public static class OperacoesMatematicas
 {
-    public (int resultadoDaAdicao, string autor) Adicionar(int valor1, int valor2)
-    {
-        var resultado = valor1 + valor2;
-
-        return (resultado, "Welisson");
-    }
-
-    public void Teste(int valor1, int valor2 = 7, string autor = "welisson")
-    {
-        Console.WriteLine(valor1 + valor2);
-        Console.WriteLine(autor);
-    }
+    public static int Adicionar(int valor1, int valor2) => valor1 + valor2;
+    public static int Subtrair() => 1;
 }
--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -4,18 +4,6 @@ class Program
 {
     static void Main()
     {
-        var carro = new Carro("Lamborghino")
-        {
-            LancadoEm = new DateOnly(2021, 01, 01),
-            Cor = Cor.Azul
-        };
-
-        var carro2 = new Carro("Porche")
-        {
-            LancadoEm = new DateOnly(2019, 01, 01),
-            Cor = Cor.Vermelho
-        };
-
-        carro.NomeDoModelo();
+        
     }
 }
