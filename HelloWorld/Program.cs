5128851caaa8974a0cb605e1f0dc4f3ebd22ddd9
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-21 13:14:48 +0000

    Else If Parte 1

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,8 +2,33 @@
 
 class Program
 {
+    enum Cores
+    {
+        Vermelho,
+        Azul,
+        Amarelo
+    }
+
     static void Main()
     {
-        
+       int numero = 0; 
+       double saldo = 100.50; 
+       bool ativo = true;
+       string autor = "welisson";
+       List<int> lista = new List<int> {1, 7};
+       Cores cor = Cores.Azul;
+
+       /*
+        > Maior
+        < Menor
+        >= Maior ou Igual
+        != diferente
+        == Igualdade
+       */
+
+       if (cor == Cores.Amarelo)
+       {
+            Console.WriteLine("ENTROUUU");
+       }
     }
 }
