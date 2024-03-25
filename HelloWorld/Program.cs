c5e29e637342977a8c785fa5455e16746f384f7c
Danilo O. Pinheiro <64677271+daniloopinheiro@users.noreply.github.com>
2024-03-25 01:31:15 +0000

    Condicional Ternario

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -2,33 +2,12 @@
 
 class Program
 {
-    enum Cores
-    {
-        Vermelho,
-        Azul,
-        Amarelo
-    }
-
     static void Main()
     {
-       int numero = 0; 
-       double saldo = 100.50; 
-       bool ativo = true;
-       string autor = "welisson";
-       List<int> lista = new List<int> {1, 7};
-       Cores cor = Cores.Azul;
+       int numero = 0;
 
-       /*
-        > Maior
-        < Menor
-        >= Maior ou Igual
-        != diferente
-        == Igualdade
-       */
+       string autor = numero = 7 || numero > 100 ? "welisson" : "willian";
 
-        if(numero == 0 && (saldo > 100.0) || ativo)
-        {
-            Console.WriteLine("ENTROUUU");
-        }
+       Console.WriteLine(autor);
     }
 }
